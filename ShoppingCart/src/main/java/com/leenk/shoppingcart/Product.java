960a5ffe9ed647cb3e7d7228ac0573e6bee15973
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-05-31 17:24:40 +0200

    SC: Created Product class with constructor and fields

---
+++ ShoppingCart/src/main/java/com/leenk/shoppingcart/Product.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.leenk.shoppingcart;
+
+/**
+ *
+ * @author Other Leenks
+ */
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
