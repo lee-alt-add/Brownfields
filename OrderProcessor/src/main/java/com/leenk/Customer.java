b1c776b19408baa77a387e76f660b5bc349dd608
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-06-03 14:03:51 +0200

    OP: Added Customer class and test file

---
+++ OrderProcessor/src/main/java/com/leenk/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.leenk;
+
+/**
+ *
+ * @author Other Leenks
+ */
+public class Customer {
+    private String name;
+    
+    public Customer(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+}
---
+++ OrderProcessor/src/test/java/com/leenk/CostomerTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.leenk;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Other Leenks
+ */
+public class CostomerTest {
+    @Test
+    void getNameTest() {
+        Customer customer = new Customer("Frank");
+        assertTrue("Frank".equals(customer.getName()));
+    }
+    
+}
