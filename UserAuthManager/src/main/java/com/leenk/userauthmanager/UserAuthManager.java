00f14e74e885b07f6c4e13cdeed8782ec80c1da8
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-06-05 10:46:49 +0200

    UAM: Initial push

---
+++ UserAuthManager/src/main/java/com/leenk/userauthmanager/UserAuthManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.leenk.userauthmanager;
+
+/**
+ *
+ * @author Other Leenks
+ */
+public class UserAuthManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
