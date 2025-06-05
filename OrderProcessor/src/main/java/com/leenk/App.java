08462cb3bd3ad5d588afdec56b152de926713a8c
lee-alt-add <lindokuhlehlatshwayo@gmail.com>
2025-06-05 10:42:46 +0200

    OP: Refactored App class to use one Customer object in viewing the code implementation

--- OrderProcessor/src/main/java/com/leenk/App.java
+++ OrderProcessor/src/main/java/com/leenk/App.java
@@ -8,27 +8,21 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         Customer customer1 = new Customer("Lincoln", true);
-        Customer customer2 = new Customer("Hloni", false);
         
-        Cart cart1 = new Cart(customer1);
-        Cart cart2 = new Cart(customer2);
+        Cart cart = new Cart(customer1);
         
         List<Product> lGoogs = List.of(new Product("Cake", 139.99, 1),
                 new Product("Ballon", 25.00, 10),
                 new Product("Decoration", 250.00, 1));
         
-        List<Product> hGoogs = List.of(new Product("Drink", 21.00, 10),
-                new Product("TV", 4500.00, 1),
-                new Product("DSTV", 700.00, 1));
-        
-        cart1.add(lGoogs);
-        cart2.add(hGoogs);
+        cart.add(lGoogs);
         
         // Making Payments
-        ProcessPayment.process(cart1, "cash");
+        ProcessPayment.process(cart, "cash");
         
         System.out.println("\n\n\n");
+        cart.getCustomer().setLoyalty(false);
         
-        ProcessPayment.process(cart2, "credit");
+        ProcessPayment.process(cart, "credit");
     }
 }
